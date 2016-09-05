5b194dc90c4ce5f5a743c541d6fa2983dae5c31d
Tabia Schmidt <tabia@Tabias-MacBook-Pro.local>
2016-09-04 21:40:52 -0700

    Loop!

--- cyoa.py
+++ cyoa.py
@@ -1,6 +1,9 @@
+user_input = raw_input("What is your name?")
+print "Hello, " + user_input
 
 
-def story():
+notDone = True
+while notDone:
     print "You are in a locked room."
     print ("> Look around")
     print ("> Wait")
@@ -8,14 +11,11 @@ def story():
     user_input = raw_input().lower()
     if user_input == "Look around".lower():
         print "You see a door."
+        notDone = False
     elif user_input == "Wait".lower():
         print "A hatch opens. A gun falls from the ceiling!! The hatch closes."
+        notDone = False
     elif user_input == "Cry".lower():
         print "It's gonna be ok :-)"
     else:
         print "You can't do that."
-        story()
-
-user_input = raw_input("What is your name?")
-print "Hello, " + user_input
-story()
